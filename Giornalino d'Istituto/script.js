033aca068d9fd656be2f062346533aebe6df2fcd
Giovanni Cappelletto <81256117+giovanni-cappelletto@users.noreply.github.com>
2022-05-02 13:18:21 +0200

    Update script.js

--- Giornalino d'Istituto/script.js
+++ Giornalino d'Istituto/script.js
@@ -1,12 +1,20 @@
-const flkity = new Flickity('.main-gallery', {
+const options = {
     draggable: true, 
     freeScrool: true,
-    wrapAround: true,
     groupCells: 1,
     autoPlay: true,
     pauseAutoPlayOnHover: true,
     selectedAttraction: 0.01,
     friction: 0.5,
     initialIndex: 0,
-    imagesLoaded: true
-})
+    imagesLoaded: true,
+    wrapAround: true,
+}
+
+if (matchMedia('screen and (min-width: 768px)').matches) {
+    options.prevNextButtons = true
+} else {
+    options.prevNextButtons = false
+}
+
+const flkity = new Flickity('.main-gallery', options)
